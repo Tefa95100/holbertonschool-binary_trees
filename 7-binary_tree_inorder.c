b296432a6993e97e5876dcef7dfbd09c544b075a
Tefa95100 <damien.julien.95100@gmail.com>
2024-12-10 13:40:24 +0100

    Change position for use func for display in order and replace or by and in if conditions

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -8,13 +8,13 @@
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree != NULL || func != NULL)
+	if (tree != NULL && func != NULL)
 	{
 		/*Browse the tree with recursive */
 		binary_tree_inorder(tree->left, (*func));
-		binary_tree_inorder(tree->right, (*func));
-
-		/*use function with the integer*/
+		/*use function with the integer */
+		/*First pointer, data, second pointer for the order traversak*/
 		func(tree->n);
+		binary_tree_inorder(tree->right, (*func));
 	}
 }
