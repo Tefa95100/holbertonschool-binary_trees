d5d99761432fc3fbb1837c0e594e19e340411847
Ali <a.medjnoun@gmail.com>
2024-12-10 19:35:57 +0100

    correcting for betty 16-14

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -16,7 +16,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (binary_tree_is_full(tree) && binary_tree_balance(tree) == 0)
 	{
-		if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
+		if (binary_tree_is_perfect(tree->left)
+		 && binary_tree_is_perfect(tree->right))
 			return (1);
 		else if (tree->left == NULL && tree->right == NULL)
 			return (1);
