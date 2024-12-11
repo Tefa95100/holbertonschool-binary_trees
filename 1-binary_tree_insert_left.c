e1aec9204fbc75c17fa566592182dc4f4b0966b5
Tefa95100 <damien.julien.95100@gmail.com>
2024-12-11 10:19:26 +0100

    Add parameter on NULL for the opposite node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,22 +13,19 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	/*Check if parent is NULL*/
 	if (parent == NULL)
-	{
 		return (NULL);
-	}
 
 	/*Allocate memory and check if fail*/
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
 
 	/*Parametre node*/
 	new_node->n = value;
 	if (parent->left != NULL)
 		parent->left->parent = new_node;
 	new_node->left = parent->left;
+	new_node->right = NULL;
 	new_node->parent = parent;
 	parent->left = new_node;
 
