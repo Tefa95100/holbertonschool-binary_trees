5f5aa76af83d850c1df1fe640082c37df5988f9b
Ali <a.medjnoun@gmail.com>
2024-12-13 12:08:04 +0100

    Inverting height and balance function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,26 +1,6 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
-/**
- * binary_tree_balance - measure the balance factor of a binary tree
- * @tree: pointer to the root node of the tree to measure the balance factor
- * Return: balance factor of the tree, or 0 if tree is NULL
- */
-
-int binary_tree_balance(const binary_tree_t *tree)
-
-{
-	int left_height, right_height;
-
-	if (tree == NULL)
-		return (0);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	return (left_height - right_height);
-}
-
 
 /**
  * binary_tree_height - Give the height of node
@@ -49,3 +29,24 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	else
 		return (height_right + 1);
 }
+
+/**
+ * binary_tree_balance - measure the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure the balance factor
+ * Return: balance factor of the tree, or 0 if tree is NULL
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+
+{
+	int left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (left_height - right_height);
+}
+
